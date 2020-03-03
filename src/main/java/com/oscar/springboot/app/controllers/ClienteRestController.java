000f211e50a0868d3d4a66de001c78f7d329e3bb
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-03 12:24:28 -0600

    metodos actualizar y eliminar cliente

--- src/main/java/com/oscar/springboot/app/controllers/ClienteRestController.java
+++ src/main/java/com/oscar/springboot/app/controllers/ClienteRestController.java
@@ -3,12 +3,16 @@ package com.oscar.springboot.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oscar.springboot.app.models.entity.Cliente;
@@ -33,9 +37,27 @@ public class ClienteRestController {
 	}
 	
 	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente crearCliente(@RequestBody Cliente cliente) {
 		return clienteService.save(cliente);
 	}
 	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente actualizarCliente(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		return clienteService.save(clienteActual);
+	}
 	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminarCliente(@PathVariable Long id) {
+		clienteService.delete(id);
+	}
+
 }
