4f09c627787e2529f06ca49338b82e82644bea45
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-02 23:08:36 -0600

    controlador clientes

---
+++ src/main/java/com/oscar/springboot/app/controllers/ClienteRestController.java
@@ -0,0 +1,24 @@
+package com.oscar.springboot.app.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oscar.springboot.app.models.entity.Cliente;
+import com.oscar.springboot.app.models.services.IClienteService;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+	
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		return clienteService.findAll();
+	}
+}
