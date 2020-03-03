2b77ef159ffd08c39f3c663dfd2fb8f59555fd90
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-03 11:40:00 -0600

    controller para obtener crear cliente

--- src/main/java/com/oscar/springboot/app/controllers/ClienteRestController.java
+++ src/main/java/com/oscar/springboot/app/controllers/ClienteRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +26,16 @@ public class ClienteRestController {
 	public List<Cliente> listar() {
 		return clienteService.findAll();
 	}
+	
+	@GetMapping("/clientes/{id}")
+	public Cliente obtenerClienteById(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	public Cliente crearCliente(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	
 }
