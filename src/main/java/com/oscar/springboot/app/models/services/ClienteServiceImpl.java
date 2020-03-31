a21ef244f7afe560c9fc27df8a82a9e5de77229c
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-31 10:56:43 -0600

    modificando service para paginador

--- src/main/java/com/oscar/springboot/app/models/services/ClienteServiceImpl.java
+++ src/main/java/com/oscar/springboot/app/models/services/ClienteServiceImpl.java
@@ -3,6 +3,8 @@ package com.oscar.springboot.app.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +24,12 @@ public class ClienteServiceImpl implements IClienteService{
 	}
 
 	@Override
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDao.findAll(pageable);
+	}
+	
+	@Override
 	public Cliente save(Cliente cliente) {
 		return clienteDao.save(cliente);
 	}
@@ -35,5 +43,5 @@ public class ClienteServiceImpl implements IClienteService{
 	public Cliente findById(Long id) {
 		return clienteDao.findById(id).orElse(null);
 	}
-	
+
 }
--- src/main/java/com/oscar/springboot/app/models/services/IClienteService.java
+++ src/main/java/com/oscar/springboot/app/models/services/IClienteService.java
@@ -2,10 +2,14 @@ package com.oscar.springboot.app.models.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.oscar.springboot.app.models.entity.Cliente;
 
 public interface IClienteService {
 	public List<Cliente> findAll();
+	public Page<Cliente> findAll(Pageable pageable);
 	public Cliente save(Cliente cliente);
 	public void delete(Long id);
 	public Cliente findById(Long id);
