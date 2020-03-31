e64b2ba45dede6d46a9da52283f1f04616ebbc43
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-31 16:37:16 -0600

    se modifica restriccion correo y fecha

--- src/main/java/com/oscar/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/oscar/springboot/app/models/entity/Cliente.java
@@ -8,12 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -37,17 +37,13 @@ public class Cliente implements Serializable{
 	
 	@NotEmpty
 	@Email
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false, unique = false)
 	private String email;
 	
+	@NotNull(message = "no puede estar vacio")
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
-	
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
 	
 	public Long getId() {
 		return id;
