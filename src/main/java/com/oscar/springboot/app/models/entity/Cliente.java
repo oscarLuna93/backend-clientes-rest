11d2ff2c95b5a3fa87c226cd3a7bf13bbb563e9c
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-01 11:45:39 -0600

    campo foto

--- src/main/java/com/oscar/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/oscar/springboot/app/models/entity/Cliente.java
@@ -45,6 +45,8 @@ public class Cliente implements Serializable{
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
+	private String foto;
+	
 	public Long getId() {
 		return id;
 	}
@@ -74,5 +76,11 @@ public class Cliente implements Serializable{
 	}
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 }
