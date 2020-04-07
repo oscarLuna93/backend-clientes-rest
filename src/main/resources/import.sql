1803710a9303524a0d0c9aeeaba5a8a75c5a3dde
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-07 11:25:10 -0500

    correccion de datos en los roles del usuario

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -419,6 +419,6 @@ INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VA
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
 
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO `user_autorities` (user_id, role_id) VALUES (1, 1);
+INSERT INTO `user_autorities` (user_id, role_id) VALUES (2, 2);
+INSERT INTO `user_autorities` (user_id, role_id) VALUES (2, 1);
