fdb013a83e51f30626750e58acc1d27c598f785c
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-07 11:24:33 -0500

    seguridad a los endpoints con httpSecurity

--- src/main/java/com/oscar/springboot/app/auth/ResourceServerConfig.java
+++ src/main/java/com/oscar/springboot/app/auth/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.oscar.springboot.app.auth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -11,7 +12,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/api/clientes").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**").permitAll()
+		.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
+		.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
+		.antMatchers(HttpMethod.POST, "/api/clientes").hasAnyRole("ADMIN")
+		.antMatchers("/api/clientes/**").hasRole("ADMIN")
 		.anyRequest().authenticated();
 	}
 }
