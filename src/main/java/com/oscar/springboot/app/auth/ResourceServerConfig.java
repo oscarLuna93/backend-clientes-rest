852e936837ae8306df0ed097fe72ad07e663f19c
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-07 12:36:13 -0500

    se quita seguridad por httpSecurity

--- src/main/java/com/oscar/springboot/app/auth/ResourceServerConfig.java
+++ src/main/java/com/oscar/springboot/app/auth/ResourceServerConfig.java
@@ -12,11 +12,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**").permitAll()
-		.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**", "/images/**").permitAll()
+		/*.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/api/clientes").hasAnyRole("ADMIN")
-		.antMatchers("/api/clientes/**").hasRole("ADMIN")
+		.antMatchers("/api/clientes/**").hasRole("ADMIN")*/
 		.anyRequest().authenticated();
 	}
 }
