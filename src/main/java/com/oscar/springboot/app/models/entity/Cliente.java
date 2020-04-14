c912066d28cd1e849c62674ccda199dc689a9c40
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-14 11:01:36 -0500

    relacion entre cliente y factura

--- src/main/java/com/oscar/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/oscar/springboot/app/models/entity/Cliente.java
@@ -1,8 +1,11 @@
 package com.oscar.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -58,6 +62,18 @@ public class Cliente implements Serializable{
 	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 	private Region region;
 	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
+	private List<Factura> facturas;
+	
+	public Cliente() {
+		this.facturas = new ArrayList<Factura>();
+	}
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
 	public Region getRegion() {
 		return region;
 	}
