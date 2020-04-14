50a1fcbf3c37a23d5df987932539425778a1bac2
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-14 16:47:27 -0500

    eliminar datos basura y arreglar loop infinto

--- src/main/java/com/oscar/springboot/app/models/entity/ItemFactura.java
+++ src/main/java/com/oscar/springboot/app/models/entity/ItemFactura.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "facturas_items")
 public class ItemFactura implements Serializable {
@@ -22,7 +24,8 @@ public class ItemFactura implements Serializable {
 	private Long id;
 	
 	private Integer cantidad;
-	
+
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "producto_id")
 	private Producto producto; 
