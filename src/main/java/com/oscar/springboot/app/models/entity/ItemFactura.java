e142fb42e1ef1c4c1d84486bd7e482a44ef71540
Oscar Luna <oscarluna.dev93@gmail.com>
2020-04-14 12:25:11 -0500

    relacion itemFactura y producto

--- src/main/java/com/oscar/springboot/app/models/entity/ItemFactura.java
+++ src/main/java/com/oscar/springboot/app/models/entity/ItemFactura.java
@@ -3,9 +3,12 @@ package com.oscar.springboot.app.models.entity;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +22,11 @@ public class ItemFactura implements Serializable {
 	private Long id;
 	
 	private Integer cantidad;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "producto_id")
+	private Producto producto; 
+	
 	public Long getId() {
 		return id;
 	}
@@ -32,7 +40,7 @@ public class ItemFactura implements Serializable {
 		this.cantidad = cantidad;
 	}
 	
-	public Double calcularImporte() {
-		return cantidad.doubleValue();
+	public Double getImporte() {
+		return cantidad.doubleValue() * producto.getPrecio();
 	}
 }
